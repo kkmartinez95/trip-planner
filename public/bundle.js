d9716849284fd6be4053830dafd27d3f4dc4c86e
Kaitlyn Martinez <kkmartinez95@gmail.com>
2019-09-30 15:02:39 -0400

    Final

--- public/bundle.js
+++ public/bundle.js
@@ -104,7 +104,7 @@ eval("/* Mapbox GL JS is licensed under the 3-Clause BSD License. Full text of l
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("const mapboxgl = __webpack_require__(/*! mapbox-gl */ \"./node_modules/mapbox-gl/dist/mapbox-gl.js\");\n\nmapboxgl.accessToken = 'pk.eyJ1Ijoia2ttYXJ0aW5lejk1IiwiYSI6ImNrMTZtZWR5eDAwcHczbW85OGlka2ZwazAifQ.X6IhFHzQn7H3DQ-lyg45lQ';\n\nconst map = new mapboxgl.Map({\n  container: \"map\",\n  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago\n  zoom: 12, // starting zoom\n  style: \"mapbox://styles/mapbox/streets-v10\" // mapbox has lots of different map styles available.\n});\n\n\n//# sourceURL=webpack:///./src/index.js?");
+eval("const mapboxgl = __webpack_require__(/*! mapbox-gl */ \"./node_modules/mapbox-gl/dist/mapbox-gl.js\");\n\nmapboxgl.accessToken = 'pk.eyJ1Ijoia2ttYXJ0aW5lejk1IiwiYSI6ImNrMTZtZWR5eDAwcHczbW85OGlka2ZwazAifQ.X6IhFHzQn7H3DQ-lyg45lQ';\n\nconst map = new mapboxgl.Map({\n  container: \"map\",\n  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago\n  zoom: 12, // starting zoom\n  style: \"mapbox://styles/mapbox/streets-v10\" // mapbox has lots of different map styles available.\n});\n\nconst markerDomEl = document.createElement(\"div\"); // Create a new, detached DIV\nmarkerDomEl.style.width = \"32px\";\nmarkerDomEl.style.height = \"39px\";\nmarkerDomEl.style.backgroundImage = \"url(http://i.imgur.com/WbMOfMl.png)\"; //https://i.imgur.com/7kUKRLp.jpg doggie photo!! \n\nconst marker = new mapboxgl.Marker(markerDomEl).setLngLat([-74.009151, 40.705086]).addTo(map);\n\n//# sourceURL=webpack:///./src/index.js?");
 
 /***/ })
 
