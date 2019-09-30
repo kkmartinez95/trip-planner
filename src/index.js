60cd65552d5ce8768e7c4f6d9da073387a5f108e
Camryn Perry <camrynpaigeperry@gmail.com>
2019-09-30 12:48:17 -0400

    added map

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-console.log("testing world");
+const mapboxgl = require('mapbox-gl');
 
+mapboxgl.accessToken = 'pk.eyJ1Ijoia2ttYXJ0aW5lejk1IiwiYSI6ImNrMTZtZWR5eDAwcHczbW85OGlka2ZwazAifQ.X6IhFHzQn7H3DQ-lyg45lQ';
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
